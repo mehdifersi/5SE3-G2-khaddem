047380a040e078861048e977c1efe477d306412d
mehdifersi <mahdi.fersi@esprit.tn>
2023-10-29 19:06:52 +0100

    unniversite class test change

--- src/main/java/tn/esprit/spring/khaddem/entities/Universite.java
+++ src/main/java/tn/esprit/spring/khaddem/entities/Universite.java
@@ -1,6 +1,5 @@
 package tn.esprit.spring.khaddem.entities;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
