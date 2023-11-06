34d944cc87d189e46423d90acf4dd6576660aff2
DhiaHamrouni <dhia.hamrouni@esprit.tn>
2023-11-06 17:55:57 +0100

    sighs2.0

--- src/main/java/tn/esprit/spring/khaddem/repositories/EquipeRepository.java
+++ src/main/java/tn/esprit/spring/khaddem/repositories/EquipeRepository.java
@@ -2,13 +2,7 @@ package tn.esprit.spring.khaddem.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import tn.esprit.spring.khaddem.entities.Equipe;
-import tn.esprit.spring.khaddem.entities.Niveau;
-
-import java.util.ArrayList;
 
 public interface EquipeRepository extends JpaRepository<Equipe,Integer> {
-
- Equipe findByNomEquipe(String nom);
- ArrayList<Equipe> findByNiveau(Niveau niveau);
 
 }
